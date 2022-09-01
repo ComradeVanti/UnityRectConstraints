fbf8135213026769848710c9be5f660945d8a41e
ComradeVanti <kingvanti650@gmail.com>
2022-09-01 11:34:28 +0200

    refactor: simplify api

--- Runtime/Constrain.cs
+++ Runtime/Constrain.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using static Dev.ComradeVanti.RectConstraints.Util;
 
 namespace Dev.ComradeVanti.RectConstraints
@@ -7,55 +7,144 @@ namespace Dev.ComradeVanti.RectConstraints
     /// <summary>
     ///     Contains methods to constrain rectangles
     /// </summary>
-    public static partial class Constrain
+    public static class Constrain
     {
 
         private static (float Start, float Size) Axis(
-            EdgeConstraint? startAt,
-            SizeConstraint? sizeOf,
-            EdgeConstraint? endAt) =>
-            (startTo: startAt, sizeIs: sizeOf, endTo: endAt) switch
+            EdgeConstraint? startConstraint,
+            SizeConstraint? sizeConstraint,
+            EdgeConstraint? endConstraint) =>
+            (startConstraint, sizeConstraint, endConstraint) switch
             {
                 (null, null, null) =>
                     empty,
                 ({ } start, null, null) =>
-                    LineAt(start, Side.Start),
+                    Util.LineAt(start, Side.Start),
                 (null, { } size, null) =>
-                    BlockWith(size),
+                    Util.BlockWith(size),
                 ({ } start, { } size, null) =>
-                    Forward(start, size),
+                    Util.Forward(start, size),
                 (null, null, { } end) =>
-                    LineAt(end, Side.End),
+                    Util.LineAt(end, Side.End),
                 ({ } start, null, { } end) =>
-                    StretchBetween(start, end),
+                    Util.StretchBetween(start, end),
                 (null, { } size, { } end) =>
-                    Backwards(end, size),
+                    Util.Backwards(end, size),
                 ({ } start, { } size, { } end) =>
-                    CenterBetween(start, end, size)
+                    Util.CenterBetween(start, end, size)
             };
 
         /// <summary>
         ///     Create a constrained rectangle
         /// </summary>
-        /// <param name="topAt">The top constraint</param>
-        /// <param name="rightAt">The right constraint</param>
-        /// <param name="bottomAt">The bottom constraint</param>
-        /// <param name="leftAt">The left constraint</param>
-        /// <param name="widthOf">The width constraint</param>
-        /// <param name="heightOf">The height constraint</param>
+        /// <param name="top">The top constraint</param>
+        /// <param name="right">The right constraint</param>
+        /// <param name="bottom">The bottom constraint</param>
+        /// <param name="left">The left constraint</param>
+        /// <param name="width">The width constraint</param>
+        /// <param name="height">The height constraint</param>
         /// <returns>The constrained rectangle</returns>
-        public static Rect MakeRectWith(
-            EdgeConstraint? topAt = null,
-            EdgeConstraint? rightAt = null,
-            EdgeConstraint? bottomAt = null,
-            EdgeConstraint? leftAt = null,
-            SizeConstraint? widthOf = null,
-            SizeConstraint? heightOf = null)
+        public static Rect Rectangle(
+            EdgeConstraint? top = null,
+            EdgeConstraint? right = null,
+            EdgeConstraint? bottom = null,
+            EdgeConstraint? left = null,
+            SizeConstraint? width = null,
+            SizeConstraint? height = null)
         {
-            var (x, w) = Constrain.Axis(leftAt, widthOf, rightAt);
-            var (y, h) = Constrain.Axis(topAt, heightOf, bottomAt);
+            var (x, w) = Constrain.Axis(left, width, right);
+            var (y, h) = Constrain.Axis(top, height, bottom);
 
             return new Rect(x, y, w, h);
+        }
+
+        /// <summary>
+        ///     Contains methods to constrain to top edges
+        /// </summary>
+        public static class ToTop
+        {
+
+            /// <summary>
+            ///     The top edge of the current window (game-view, editor-window, ...)
+            /// </summary>
+            public static EdgeConstraint OfWindow =>
+                new EdgeConstraint(0);
+
+            /// <summary>
+            ///     Constrains to the top edge of the given rectangle
+            /// </summary>
+            /// <param name="rect">The rectangle</param>
+            /// <returns>The created constraint</returns>
+            public static EdgeConstraint Of(Rect rect) =>
+                new EdgeConstraint(rect.yMin);
+
+        }
+
+
+        /// <summary>
+        ///     Contains methods to constrain to right edges
+        /// </summary>
+        public static class ToRight
+        {
+
+            /// <summary>
+            ///     The right edge of the current window (game-view, editor-window, ...)
+            /// </summary>
+            public static EdgeConstraint OfWindow =>
+                new EdgeConstraint(Screen.width);
+
+            /// <summary>
+            ///     Constrains to the right edge of the given rectangle
+            /// </summary>
+            /// <param name="rect">The rectangle</param>
+            /// <returns>The created constraint</returns>
+            public static EdgeConstraint Of(Rect rect) =>
+                new EdgeConstraint(rect.xMax);
+
+        }
+
+        /// <summary>
+        ///     Contains methods to constrain to bottom edges
+        /// </summary>
+        public static class ToBottom
+        {
+
+            /// <summary>
+            ///     The bottom edge of the current window (game-view, editor-window, ...)
+            /// </summary>
+            public static EdgeConstraint OfWindow =>
+                new EdgeConstraint(Screen.height);
+
+            /// <summary>
+            ///     Constrains to the bottom edge of the given rectangle
+            /// </summary>
+            /// <param name="rect">The rectangle</param>
+            /// <returns>The created constraint</returns>
+            public static EdgeConstraint Of(Rect rect) =>
+                new EdgeConstraint(rect.yMax);
+
+        }
+
+        /// <summary>
+        ///     Contains methods to constrain to left edges
+        /// </summary>
+        public static class ToLeft
+        {
+
+            /// <summary>
+            ///     The left edge of the current window (game-view, editor-window, ...)
+            /// </summary>
+            public static EdgeConstraint OfWindow =>
+                new EdgeConstraint(0);
+
+            /// <summary>
+            ///     Constrains to the left edge of the given rectangle
+            /// </summary>
+            /// <param name="rect">The rectangle</param>
+            /// <returns>The created constraint</returns>
+            public static EdgeConstraint Of(Rect rect) =>
+                new EdgeConstraint(rect.xMin);
+
         }
 
     }
